045df840a07535babcdbe9a963394689e5674ace
questras <michu123qwe@gmail.com>
2021-04-23 14:34:08 +0200

    Add custom exceptions for handling services' errors.

---
+++ repositories_app/exceptions.py
@@ -0,0 +1,37 @@
+"""
+Module with custom exceptions that might raise during
+application runtime.
+"""
+
+
+class UserNotFoundServiceException(Exception):
+    """
+    Exception raised in a service, when call to a service
+    was not successful, because desired user was not found.
+    """
+
+    def __init__(self):
+        message = "Desired user not found."
+        super().__init__(message)
+
+
+class ApiRateLimitExceededServiceException(Exception):
+    """
+    Exception raised in a service, when call to a service
+    was not successful due to exceeded limit of calls to api.
+    """
+
+    def __init__(self):
+        message = "Api rate limit exceeded."
+        super().__init__(message)
+
+
+class BadCredentialsServiceException(Exception):
+    """
+    Exception raised in a service, when call to a service
+    was not successful due to incorrect credentials.
+    """
+
+    def __init__(self):
+        message = "Bad credentials were provided."
+        super().__init__(message)
